a2539ad5df2d1e1b5d7156ca26c0ecd34c6c0142
zz-zhang <zz_zhang@f.ait.kyushu-u.ac.jp>
2019-03-12 13:34:19 +0900

    problem 30 solved

--- source code/30. Substring with Concatenation of All Words.py
+++ source code/30. Substring with Concatenation of All Words.py
@@ -9,12 +9,24 @@ class Solution:
         word_length = len(words[0])
         start_index = 0
 
+        # while start_index + (dict_length * word_length) <= len(s):
+        #     temp_s = s[start_index : start_index + (dict_length * word_length)]
+        #     for word in words:
+        #         if word not in temp_s:
+        #             break
+        #         temp_s = temp_s[ : temp_s.index(word)] + temp_s[temp_s.index(word) + word_length : ]
+        #     else:
+        #         res.append(start_index)
+        #     start_index += 1
+        # return res
         while start_index + (dict_length * word_length) <= len(s):
             temp_s = s[start_index : start_index + (dict_length * word_length)]
+            s_list = [temp_s[i:i+word_length] for i in range(0, len(temp_s), word_length)]
             for word in words:
-                if word not in temp_s:
+                if word not in s_list:
                     break
-                temp_s = temp_s[ : temp_s.index(word)] + temp_s[temp_s.index(word) + word_length : ]
+                # s_list = s_list[:s_list.index(word)] + s_list[s_list.index(word) + word_length:]
+                s_list.pop(s_list.index(word))
             else:
                 res.append(start_index)
             start_index += 1
@@ -22,6 +34,6 @@ class Solution:
 
 if __name__ == '__main__':
     s = Solution()
-    S = "ababaab"
-    words = ["ab", "ba", "ba"]
+    S = "wordgoodgoodgoodbestword"
+    words = ["word","good","best","word"]
     print(s.findSubstring(S, words))
