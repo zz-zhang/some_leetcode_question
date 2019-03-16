51ae7cb260d2eb44d06211386b979e9537739679
zz-zhang <zz_zhang@f.ait.kyushu-u.ac.jp>
2019-03-16 14:45:45 +0900

    problem 72 solved, referenced other's answer

--- source code/72. Edit Distance.py
+++ source code/72. Edit Distance.py
@@ -1,79 +1,26 @@
-import queue
-
 class Solution:
-    def replace(self, str, i, dict, j):
-        res = ''
-        res[i] = dict[j]
-        return res
-
-    def removeZero(self, str):
-        res = ""
-        for i in str:
-            if i != '0':
-                res.append(i)
-        return res
-
-    def similar(self, str1, str2):
-        res = 0
-        s1 = ""
-        s2 = ""
-        if len(str1) < len(str2):
-            s1 = str2
-            s2 = str1
-        else:
-            s1 = str1
-            s2 = str2
-
-        for i in range(0, len(s1)):
-            sim = 0
-            for j in range(0, len(s2)):
-                if i + j >= len(s1):
-                    break
-                if s1[i + j] == s2[i + j]:
-                    sim += 1
-            if sim > res:
-                res = sim
-        return res
-
     def minDistance(self, word1, word2):
         """
         :type word1: str
         :type word2: str
         :rtype: int
         """
-        target = word2
-        dict = target + "0"
-        q = queue.Queue()
-        q.put(word1)
-        qdepth = queue.Queue()
-        qdepth.put(0)
-        while not q.empty():
-            str = q.get()
-            dep = qdepth.get()
-            if str == target:
-                return dep
+        dp = [[0 for _ in range(0, len(word2) + 1)] for __ in range(0, len(word1) + 1)]
+        for i in range(0, len(word1) + 1):
+            dp[i][0] = i
+        for j in range(0, len(word2) + 1):
+            dp[0][j] = j
+        for i in range(1, len(word1) + 1):
+            for j in range(1, len(word2) + 1):
+                dp[i][j] = min(dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1] + (word1[i - 1] != word2[j - 1]))
+                # if word1[i - 1] != word2[j - 1]:
+                #     dp[i][j] = min(dp[i][j], dp[i - 1][j - 1] + 1)
+        return dp[-1][-1]
 
-            for i in range(0, len(str)):
-                maxSim = 0
-                simList = []
-                strList = []
-
-                for j in range(0, len(dict)):
-                    strTemp = self.replace(str, i, dict, j)
-                    strTemp = self.removeZero(strTemp)
-                    sim = self.similar(target, strTemp)
-                    simList.append(sim)
-                    strList.append(strTemp)
-                for k in range(0, simList):
-                    if simList[k] == maxSim:
-                        q.put(strList[k])
-                        qdepth.put(dep + 1)
-            q.pop()
-            qdepth.pop()
 
 
 if __name__ == '__main__':
     sol = Solution()
-    word1 = 'horse'
-    word2 = 'ros'
+    word1 = 'intention'
+    word2 = 'execution'
     print(sol.minDistance(word1, word2))
