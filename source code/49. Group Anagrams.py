478e10f4d274ec16e79664c27dbc70f8b4fbe1f5
zz-zhang <zz_zhang@f.ait.kyushu-u.ac.jp>
2019-08-13 14:01:38 +0900

    49 ac

--- source code/49. Group Anagrams.py
+++ source code/49. Group Anagrams.py
@@ -31,23 +31,28 @@ class Solution:
         # return result
         # ----------
         sorted_strs = [s for s in strs]
+        strs_pair = []
         for index in range(0, len(strs)):
             sorted_strs[index] = ''.join(sorted(strs[index]))
-        # print(sorted_strs)
+            strs_pair.append((strs[index], sorted_strs[index]))
+
+        strs_pair = sorted(strs_pair, key=lambda x:(x[1], x[0]))
+        # print(strs_pair)
 
         result = []
-        used = []
-        for i in range(0,len(strs)):
-            if sorted_strs[i] not in used:
-                used.append(sorted_strs[i])
-                sub_res = [strs[i]]
-                for j in range(i + 1, len(strs)):
-                    if sorted_strs[i] == sorted_strs[j]:
-                        sub_res.append(strs[j])
+        sub_res = [strs_pair[0][0]]
+        index = 1
+        while index < len(strs_pair):
+            if strs_pair[index][1] == strs_pair[index - 1][1]:
+                sub_res.append(strs_pair[index][0])
+            else:
                 result.append(sub_res)
+                sub_res = [strs_pair[index][0]]
+            index += 1
+        result.append(sub_res)
         return result
 
 if __name__ == '__main__':
     sol = Solution()
-    strs = ["","", "abc", "cba", "abd"]
+    strs =  ["eat", "tea", "tan", "ate", "nat", "bat"]
     print(sol.groupAnagrams(strs))
