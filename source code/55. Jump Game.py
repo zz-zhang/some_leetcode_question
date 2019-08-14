9c579a7e398d553d94b9155a70f3b6404c8fde7e
zz-zhang <zz_zhang@f.ait.kyushu-u.ac.jp>
2019-08-14 11:04:49 +0900

    55 ac
    accessable: for an index, if it is unaccessable, mark it, and this index will not be accessed next time to save time.

--- source code/55. Jump Game.py
+++ source code/55. Jump Game.py
@@ -1,21 +1,21 @@
 class Solution:
+    def reachable(self, nums, target, max_length, accessable):
+        # print(target)
+        if target == 0:
+            return True
+        for i in range(max_length, 0, -1):
+            if target - i >= 0 and nums[target - i] >= i and accessable[target - i] == 1:
+                if self.reachable(nums, target - i, max_length, accessable):
+                    return True
+        accessable[target] = 0
+        return False
+
     def canJump(self, nums) -> bool:
-        if len(nums) == 0:
-            return True
-        reachable = [0 for _ in nums]
-        reachable[0] = 1
-        for i in range(0, len(nums) - 1):
-            if reachable[i] == 1:
-                for j in range(1, nums[i] + 1):
-                    if i + j < len(nums):
-                        reachable[i + j] = 1
-        # print(reachable)
-        if reachable[len(nums) - 1] == 1:
-            return True
-        else:
-            return False
+        max_length = max(nums)
+        accessable = [1 for _ in nums]
+        return self.reachable(nums, len(nums) - 1, max_length, accessable)
 
 if __name__ == '__main__':
     sol = Solution()
-    nums = [2, 0]
+    nums = [2,0,6,9,8,4,5,0,8,9,1,2,9,6,8,8,0,6,3,1,2,2,1,2,6,5,3,1,2,2,6,4,2,4,3,0,0,0,3,8,2,4,0,1,2,0,1,4,6,5,8,0,7,9,3,4,6,6,5,8,9,3,4,3,7,0,4,9,0,9,8,4,3,0,7,7,1,9,1,9,4,9,0,1,9,5,7,7,1,5,8,2,8,2,6,8,2,2,7,5,1,7,9,6]
     print(sol.canJump(nums))
