5b7ba3a60fb58d6ccc1d3066d791be5b02808431
zz-zhang <zz_zhang@f.ait.kyushu-u.ac.jp>
2019-08-19 11:46:43 +0900

    91 ac

--- source code/91. Decode Ways.py
+++ source code/91. Decode Ways.py
@@ -1,20 +1,26 @@
 class Solution:
-    def count(self, s):
-        if len(s) == 0:
-            self.res += 1
-            return
-        for length in range(1, 3):
-            if length <= len(s):
-                if s[:length] in self.dic:
-                    self.count(s[length:])
-
     def numDecodings(self, s: str) -> int:
-        self.dic = [str(_) for _ in range(1, 27)]
-        self.res = 0
-        self.count(s)
-        return self.res
+        if s[0] == '0':
+            return 0
+        dic = [str(_) for _ in range(1, 27)]
+        dp = [0 for item in s]
+        single_end = [0 for item in s]
+        single_end[0] = 1
+        dp[0] = 1
+        for i in range(1, len(s)):
+            if s[i: i + 1] in dic:
+                dp[i] = dp[i - 1]
+                single_end[i] = dp[i - 1]
+            else:
+                single_end[i] = 0
+            if s[i - 1:i + 1] in dic:
+                dp[i] += single_end[i - 1]
+        # print(dp)
+        # print(single_end)
+        return dp[-1]
+
 
 if __name__ == '__main__':
     sol = Solution()
-    s = '37'
+    s = '1012'
     print(sol.numDecodings(s))
