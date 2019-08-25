d611d5b09478f1ceb20d3a6b33a08e6bcf0334dc
zz-zhang <zz_zhang@f.ait.kyushu-u.ac.jp>
2019-08-25 16:49:51 +0900

    87 ac

--- source code/87. Scramble String.py
+++ source code/87. Scramble String.py
@@ -1,50 +1,21 @@
 class Solution:
-
-    def is_continue(self, sub_num):
-        sort_num = sorted(sub_num)
-        for index in range(1, len(sort_num)):
-            if sort_num[index] - sort_num[index - 1] != 1:
-                return False
-        return True
-
-    def is_legal(self, sub_num):
-        if len(sub_num) == 1:
-            return True
-        if len(sub_num) == 2:
-            if abs(sub_num[0] - sub_num[1]) == 1:
-                return True
-            else:
-                return False
-        else:
-            flag = False
-            for index in range(1, len(sub_num) - 1):
-                if self.is_legal(sub_num[:index]) and self.is_legal(sub_num[index:]):
-                    flag = True
-                    break
-            if flag and self.is_continue(sub_num):
-                return True
-            else:
-                return False
-
-
     def isScramble(self, s1: str, s2: str) -> bool:
-        num_dict = {}
-        for i, c in enumerate(s1):
-            num_dict[c] = i
-        # print(num_dict)
-        s2_num = []
-        for c in s2:
-            if c in num_dict:
-                s2_num.append(num_dict[c])
-            else:
-                return False
-        # print(s2_num)
-        return self.is_legal(s2_num)
+        if s1 == s2:
+            return True
+        if len(s1) != len(s2):
+            return False
+        if sorted(s1) != sorted(s2):
+            return False
+        for i in range(1, len(s1)):
+            if (self.isScramble(s1[:i], s2[:i]) and self.isScramble(s1[i:], s2[i:])) or \
+                (self.isScramble(s1[:i], s2[-i:]) and self.isScramble(s1[i:], s2[:-1])):
+                return True
+        return False
 
 
 
 if __name__ == '__main__':
     sol = Solution()
-    str1 = 'great'
-    str2 = 'rgeat'
+    str1 = 'abab'
+    str2 = 'bbaa'
     print(sol.isScramble(str1, str2))
