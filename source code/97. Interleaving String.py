b60e1da886328312eb3aa1ab7e16c14a64f59d6a
zz-zhang <zz_zhang@f.ait.kyushu-u.ac.jp>
2019-08-27 16:31:41 +0900

    97 ac

--- source code/97. Interleaving String.py
+++ source code/97. Interleaving String.py
@@ -1,33 +1,23 @@
 class Solution:
     def isInterleave(self, s1, s2, s3):
-        if s1 + s2 == s3 or s2 + s1 == s3:
-            return True
-        p1 = 0
-        p2 = 0
-        for p3 in range(len(s3)):
-            if (p1 < len(s1) and s3[p3] == s1[p1]) or (p2 < len(s2) and s3[p3] == s2[p2]):
-                if p1 < len(s1) and p2 < len(s2) and s1[p1] == s2[p2]:
-                    if self.isInterleave(s1[p1 + 1:], s2[p2:], s3[p3 + 1:]):
-                        return True
-                    elif self.isInterleave(s1[p1:], s2[p2 + 1:], s3[p3 + 1:]):
-                        return True
-                    else:
-                        return False
-                elif p1 < len(s1) and s1[p1] == s3[p3]:
-                    p1 += 1
-                elif p2 < len(s2) and s2[p2] == s3[p3]:
-                    p2 += 1
-                else:
-                    return False
-            else:
-                return False
-        if p1 == len(s1) and p2 == len(s2):
-            return True
-        return False
+        if len(s1) + len(s2) != len(s3):
+            return False
+        dp = [[False for _ in range(len(s2) + 1)] for __ in range(len(s1) + 1)]
+        dp[0][0] = True
+        for i in range(len(s1) + 1):
+            for j in range(len(s2) + 1):
+                if i == j == 0:
+                    continue
+                if dp[i - 1][j] and s1[i - 1] == s3[i + j - 1]:
+                    dp[i][j] = True
+                elif dp[i][j - 1] and s2[j - 1] == s3[i + j - 1]:
+                    dp[i][j] = True
+        return dp[-1][-1]
 
 if __name__ == '__main__':
     sol = Solution()
     s1 = "ab"
-    s2 = "c"
-    s3 = "c"
+    s2 = "bc"
+    s3 = "babc"
+
     print(sol.isInterleave(s1, s2, s3))
