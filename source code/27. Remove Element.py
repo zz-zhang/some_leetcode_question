6265149cac214d7b8166592d8e581bd5bac76a0f
zz-zhang <zzz.revival@gmail.com>
2023-07-06 13:40:39 +0900

    modify 27

--- source code/27. Remove Element.py
+++ source code/27. Remove Element.py
@@ -1,17 +1,23 @@
+from typing import List
+from random import randint
 class Solution:
-    def removeElement(self, nums, val):
-        """
-        :type nums: List[int]
-        :type val: int
-        :rtype: int
-        """
-        i = 0
-        while val in nums:
-            nums.remove(val)
-        return len(nums)
+    def removeElement(self, nums: List[int], val: int) -> int:
+        src, tgt = 0, 0
+        while src < len(nums):
+            if nums[src] != val:
+                nums[tgt] = nums[src]
+                tgt += 1
+            src += 1
+
+        # print(nums, src, tgt)
+        return tgt
 
 if __name__ == '__main__':
     sol = Solution()
-    nums = [3,2,2,3]
-    val = 3
+    nums = [0, 0, 1]
+    val = 0
+    # nums = [randint(0, 50) for _ in range(100)]
+    # val = nums[0]
+    print(nums, val)
     print(sol.removeElement(nums, val))
+    print(nums)
